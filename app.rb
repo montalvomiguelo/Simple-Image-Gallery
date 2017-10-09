d2b51fc4110956f369aa03c080a8bad1790ca780
imontalvomiguel <ubaldo.im@gmail.com>
2017-10-08 19:39:31 -0500

    basic authorization

--- app.rb
+++ app.rb
@@ -2,14 +2,34 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get "/" do
+    @auth = authorized?
     @images = Image.all
     erb :index
   end
 
   post '/images' do
+    protected!
     @image = Image.new params[:image]
     @image.save
 
     redirect '/'
+  end
+
+  get '/auth' do
+    protected!
+    redirect '/'
+  end
+
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+    end
   end
 end
